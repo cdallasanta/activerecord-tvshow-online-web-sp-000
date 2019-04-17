bd88af44718c63a10ff334e4f64473bee6aaddcc
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 23:07:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190417230557_create_shows.rb
+++ db/migrate/20190417230557_create_shows.rb
@@ -1,6 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      t
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
+    end
   end
 end
