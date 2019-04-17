f39ae36408fef3e2ed7b08d08f89cd581e26cef1
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 23:16:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,23 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def self.most_popular
+    self.maximum(:rating)
+  end
+
+  def self.highest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular
+    self.minimum(:rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:ratings)
+  end
+
+  def self.popular_shows
+    self.where()
 end
